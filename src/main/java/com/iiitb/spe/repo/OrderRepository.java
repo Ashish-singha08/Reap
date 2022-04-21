fec1a0f2d0a15b31fbbe00e65c713a8fe09c8e80
Ashish-singha08 <ashishsinghal780@gmail.com>
2022-04-21 12:06:46 +0530

    Added Order Repo

---
+++ src/main/java/com/iiitb/spe/repo/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.iiitb.spe.repo;
+
+import com.iiitb.spe.model.entities.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
+
+    List<OrderEntity> findById(long id);
+}
